9a7ddbda57ec630b7b3824368902f519be2fa821
Adrian <adrian.talonia11@gmail.com>
2022-06-29 22:23:43 -0500

    add springboot module

---
+++ kotlin_springboot_api/src/main/kotlin/com/atr/kotlin_springboot_api/KotlinSpringbootApiApplication.kt
@@ -0,0 +1,11 @@
+package com.atr.kotlin_springboot_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringbootApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringbootApiApplication>(*args)
+}
---
+++ kotlin_springboot_api/src/test/kotlin/com/atr/kotlin_springboot_api/KotlinSpringbootApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.atr.kotlin_springboot_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringbootApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
