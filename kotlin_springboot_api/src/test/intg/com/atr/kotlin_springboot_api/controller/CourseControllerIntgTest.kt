c832576c683ab5963b7dbcdefde1c21bd1c1c448
Adrian <adrian.talonia11@gmail.com>
2022-07-01 23:58:44 -0500

    post test

---
+++ kotlin_springboot_api/src/test/intg/com/atr/kotlin_springboot_api/controller/CourseControllerIntgTest.kt
@@ -0,0 +1,39 @@
+package com.atr.kotlin_springboot_api.controller
+
+import com.atr.kotlin_springboot_api.dto.CourseDTO
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
+class CourseControllerIntgTest {
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @Test
+    fun addCourse() {
+        var courseDTO = CourseDTO(null, "BUild Resfult APIs using Springboot and Kotlin", "Development")
+
+        var savedCourseDTO = webTestClient
+            .post()
+            .uri("/v1/courses")
+            .bodyValue(courseDTO)
+            .exchange()
+            .expectStatus().isCreated
+            .expectBody(CourseDTO::class.java)
+            .returnResult()
+            .responseBody
+
+        Assertions.assertTrue {
+            savedCourseDTO!!.id != null
+        }
+    }
+
+}
